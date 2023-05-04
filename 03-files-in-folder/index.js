b48d6f9496853001cbc7874b6e6162844e805124
github-MaksimDolgonosov <max_air@bk.ru>
2023-05-04 11:37:40 +0300

    feat: add solution of 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-// const content = fs.readdir('secret-folder');
-// console.log(content);
 
 fs.readdir(path.join(__dirname, '/secret-folder'), { encoding: 'utf8', withFileTypes: true }, (err, files) => {
   if (err) throw err;
   files.forEach(file => {
-    console.log(path.extname(file.name));
+    fs.stat(path.join(__dirname, '/secret-folder', file.name), (err, stats) => {
+      if (err) throw err;
+      if (stats.isFile()) {
+        console.log(`${path.basename(file.name, path.extname(file.name))} - ${path.extname(file.name).replace(/./,"")} - ${(stats.size/1024).toFixed(4)}kb`)
+      }
+
+    })
+
   })
 
 });
+
