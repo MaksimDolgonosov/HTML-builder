fa45ed8b6ce63c442e750e45dcb965081204a967
github-MaksimDolgonosov <max_air@bk.ru>
2023-05-04 16:59:38 +0300

    feat: working on solution of 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+const { mkdir } = require('fs/promises');
+const { rmdir } = require('fs/promises');
+const { copyFile } = require('fs/promises');
+const path = require('path');
 
+//mkdir(path.join(__dirname, 'files-copy'), { recursive: true, });
+
+readdir(path.join(__dirname, '/files-copy'), (err, filess) => {
+  if (err) {
+    mkdir(path.join(__dirname, 'files-copy'), { recursive: true, });
+  } else {
+    rmdir(path.join(__dirname, 'files-copy'), err => {
+      if (err) throw err;
+    });
+
+  }
+
+})
+readdir(path.join(__dirname, '/files'), { encoding: 'utf8', withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+
+
+  // readdir(path.join(__dirname, '/files-copy'), (err,filess)=>{
+  //   if (err) {
+  //     mkdir(path.join(__dirname, 'files-copy'), { recursive: true, });
+  //   } else {
+  //     rmdir(path.join(__dirname, 'files-copy'), err => {
+  //       if(err) throw err; 
+  //    });
+
+  //   }
+
+  // })
+
+  files.forEach(file => {
+    copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+      if (err) throw err;
+    })
+  })
+
+});
